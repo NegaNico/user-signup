392359f61f67971a7bba3522b2463f61d363b8bc
Nicolas Little <nlittle@Nicolass-MacBook-Pro.local>
2019-04-15 18:54:38 -0500

    completed?

--- main.py
+++ main.py
@@ -22,15 +22,14 @@ def validate_form():
     email_error = ''
     
     if len(username) < 3 or len(username) > 20:
-        username_error = "That's not a valid username (must be between 3-20 characters long)"
-
+        username_error = "invalid username (3-20 characters long)"
     if len(password) < 3 or len(password) > 20:
-        password_error = "That's not a valid password (must be between 3-20 characters long)"
+        password_error = "invalid password (3-20 characters long)"
     
     if username == '':
-        username_error = "That's not a valid username"
+        username_error = "invalid username"
     if password == '':
-        password_error = "That's not a valid password"
+        password_error = "invalid password"
     if verify_password == '':
         verify_password_error = "Passwords don't match"
     
@@ -39,19 +38,17 @@ def validate_form():
 
     if email:
         if len(email) < 3 or len(email) > 20:
-            email_error = "That's not a valid email (must be between 3-20 characters long)"
+            email_error = "invalid email (3-20 characters long)"
 
         if ' ' in email or '@' not in email or '.' not in email:
-            email_error = "That's not a valid email"
+            email_error = "invalid email"
     
     if not username_error and not password_error and not verify_password_error and not email_error:
         return redirect('/welcome?username={0}'.format(username))
 
     else:
-        return render_template('user_signup_form.html',
-        username=username, username_error=username_error,
-        password_error=password_error,
-        verify_password_error=verify_password_error,
+        return render_template('user_signup_form.html', username=username, username_error=username_error,
+        password_error=password_error, verify_password_error=verify_password_error, 
         email=email, email_error=email_error)
 
 @app.route("/welcome")
