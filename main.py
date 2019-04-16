e50c88b3054d56757b4c4f1868934fa2c743981c
Nicolas Little <nlittle@Nicolass-MacBook-Pro.local>
2019-04-15 19:44:52 -0500

    completed again hopefully color works now

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    return render_template('user_signup_form.html')
+    return render_template('user_signup.html')
 
 @app.route("/", methods=['POST'])
 def validate_form():
@@ -32,28 +32,25 @@ def validate_form():
         password_error = "invalid password"
     if verify_password == '':
         verify_password_error = "Passwords don't match"
-    
     if password != verify_password:
         verify_password_error = "Passwords don't match"        
 
     if email:
         if len(email) < 3 or len(email) > 20:
             email_error = "invalid email (3-20 characters long)"
-
         if ' ' in email or '@' not in email or '.' not in email:
             email_error = "invalid email"
     
     if not username_error and not password_error and not verify_password_error and not email_error:
         return redirect('/welcome?username={0}'.format(username))
-
     else:
-        return render_template('user_signup_form.html', username=username, username_error=username_error,
+        return render_template('user_signup.html', username=username, username_error=username_error,
         password_error=password_error, verify_password_error=verify_password_error, 
         email=email, email_error=email_error)
 
 @app.route("/welcome")
 def welcome():
     username = request.args.get('username')
-    return render_template("welcome_form.html", username=username)
+    return render_template("welcome.html", username=username)
 
 app.run()
